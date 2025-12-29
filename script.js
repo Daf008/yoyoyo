82082b4ec9df789ae8dbd6d62a23d09e08a2a822
Daffa Fauzan Putra Somantri <120447552+Daf008@users.noreply.github.com>
2025-12-29 12:59:38 +0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("ctaBtn").addEventListener("click", function () {
-        alert("Button berhasil diklik!");
+        alert("Kelas gesss!");
     });
 });
+
