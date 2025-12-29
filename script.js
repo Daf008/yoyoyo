8f94b8b39fec5fb8f11a6e147d2486fd59b24c14
Daffa Fauzan Putra Somantri <120447552+Daf008@users.noreply.github.com>
2025-12-29 13:05:53 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("ctaBtn").addEventListener("click", function () {
-        alert("Kelas gesss!");
-    });
-});
-
