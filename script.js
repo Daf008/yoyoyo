3b2672aaedb103660d36abe12d2b0dd6a562448b
Daffa Fauzan Putra Somantri <120447552+Daf008@users.noreply.github.com>
2025-12-29 13:06:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("DEPLOY TEST v2");
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("ctaBtn").addEventListener("click", function () {
+        alert("Kelas gesss!");
+    });
+});
